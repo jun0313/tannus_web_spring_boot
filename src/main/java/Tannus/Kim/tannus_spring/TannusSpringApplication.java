e2e8793ec3d1b8be00dd9e062b6c6c87a62e8cf7
JunTae Kim <luppy1004@naver.com>
2025-06-24 15:14:54 +0900

    feat: first commit

---
+++ src/main/java/Tannus/Kim/tannus_spring/TannusSpringApplication.java
@@ -0,0 +1,13 @@
+package Tannus.Kim.tannus_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TannusSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TannusSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tannus/Kim/tannus_spring/TannusSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Tannus.Kim.tannus_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TannusSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
